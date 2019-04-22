7e061e5512ac4f151a60511b770b2c42daab710e
Luca Morandini <lmorandini@ieee.org>
2019-04-22 11:04:46 +1000

    - Deleted SOAP sample, as it is no longer part of the course

--- soapsample/clientWeather.js
+++
@@ -1,9 +0,0 @@
-require("soap").createClient(
-    "http://www.webservicex.com/globalweather.asmx?WSDL",
-    function(err, client) {
-      client.GlobalWeather.GlobalWeatherSoap12.GetCitiesByCountry({
-        CountryName : "Australia"
-      }, function(err, result) {
-        console.log(result.GetCitiesByCountryResult);
-      });
-    });
